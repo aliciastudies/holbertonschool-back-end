aec7fff6c3e3b951c9fcfa263c1bbef74bc8a034
aliciastudies <6722@holbertonstudents.com>
2023-12-18 18:11:32 +1100

    style: added comments and more explicit var names

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -6,14 +6,20 @@ import sys
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com"
+    base_url = "https://jsonplaceholder.typicode.com"
     employee_id = sys.argv[1]
+    # get ID from command line
     user = requests.get(url + "/users/{}".format(employee_id)).json()
     todos = requests.get(url + "/users/" + employee_id + "/todos").json()
+    # get user infro and TODO lists for specified user
 
     task_details = [{"task": todo["title"], "completed": todo["completed"],
                     "username": user["username"]} for todo in todos]
+    # create list of dict with task info
+
     employee_data = {employee_id: task_details}
+    # create dict with ID as key and tasks as values
 
     with open(f"{employee_id}.json", "w") as outfile:
         json.dump(employee_data, outfile)
+    # write dict to JSON file
